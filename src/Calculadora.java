b0b586e1886ba8978497a15037352c96d91740f2
snaphuman <fhernandezn@gmail.com>
2016-01-31 15:11:19 -0500

    inicia el programa y define mensaje de bienvenida al usuario

---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+/**
+ * Created by snaphuman on 1/27/16.
+ */
+public class Calculadora {
+
+    public static void main (String[] args) {
+
+        String $msg;
+
+        $msg = "Bienvenido a la calculadora estadística \n";
+        $msg += "Este programa le permitirá definir una lista de valores cuya longitud \n";
+        $msg += "será indicada por el usted. \n";
+        $msg += "Una vez definidos los valores, usted podrá elegir el tipo de operación \n";
+        $msg += "estadística que desea aplicar sobre los valores";
+
+        System.out.println($msg);
+
+    }
+}
